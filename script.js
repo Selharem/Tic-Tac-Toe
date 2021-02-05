d8c16df6494a38804caad70fefc6ba46634c0f1c
Soufiane Elharem <44909504+Selharem@users.noreply.github.com>
2021-02-05 23:18:01 +0100

    Update script.js
    fix some bugs

--- script.js
+++ script.js
@@ -1,112 +1,78 @@
-var playedXY = [0,0,0,0,0,0,0,0,0]
-var play = false;
+var playedX = [0,0,0,0,0,0,0,0,0]; //Player 1
+var playedY = [0,0,0,0,0,0,0,0,0]; //Player 1
+var switched = false; //for switching turns
 var y;
-var playedX = [0,0,0,0,0,0,0,0,0]
-var playedY = [0,0,0,0,0,0,0,0,0]
-var EndGame = false;
-
-document.getElementsByTagName('table').style.color = 'red';
-
+var winner = null;
 
 function choose(x) {
     y = x;
-    if(play == true){
+    if(switched == true){
+        winner = 'Black';
         displayX();
-        verify(playedX);
     }
     else{
+        winner = 'White';
         displayY();
         verify(playedY);
     }
 }
 
-console.log(playedX);
-
-function displayX() {
+function displayX() { //change the color when clicking if the other player didn't clicked it  
     if(playedY[y]==0){
-        document.getElementById(y).style.background = 'white';
-        play = !(play);
+        document.getElementById(y).style.background = 'white'; 
+        switched = !(switched);
         playedX[y]=1;
-        playedXY[y]=1;
     }
 }
 
-function displayY() {
+function displayY() { //change the color when clicking if the other player didn't clicked it 
     if(playedX[y]==0){
         document.getElementById(y).style.background = 'black';
-        play = !(play);
+        switched = !(switched);
         playedY[y]=1;
-        playedXY[y]=1;
     }
 }
 
-function verify(arrayP) {
+function verify(arrayP) { //verify the 8 possibilities of winning the game
+
     if(arrayP[0]==1){
         if(arrayP[1]==1 & arrayP[2]==1){
-            confirm('play again');
-            //location.reload();
-            //document.getElementById('table').style.background = 'aqua';
-            resetColor();
-            EndGame = true;
+            confirm(winner + ' wins.' + ' play again?');
+            location.reload();
         }if(arrayP[3]==1 & arrayP[6]==1){
-            confirm('play again');
-            //location.reload();
-            //document.getElementById('table').style.background = 'aqua';
-            resetColor();
-            EndGame = true;
+            confirm(winner + ' wins.' + ' play again?');
+            location.reload();
         }if(arrayP[4]==1 & arrayP[8]==1){
-            confirm('play again');
-            resetColor();
-            //document.getElementById('table').style.background = 'aqua';
-            //location.reload();
-            EndGame = true;
+            confirm(winner + ' wins.' + ' play again?');
+            location.reload();
         }
     }
     
     if(arrayP[2]==1){
         if(arrayP[5]==1 & arrayP[8]==1){
-            confirm('play again');
-            resetColor();
-            EndGame = true;
+            confirm(winner + ' wins.' + ' play again?');
+            location.reload();
         }if(arrayP[4]==1 & arrayP[6]==1){
-            confirm('play again');
-            resetColor();
-            EndGame = true;
+            confirm(winner + ' wins.' + ' play again?');
+            location.reload();
         }
     }
     
     if(arrayP[4]==1){
         if(arrayP[1]==1 & arrayP[7]==1){
-            confirm('play again');
-            resetColor();
-            EndGame = true;
+            confirm(winner + ' wins.' + ' play again?');
+            location.reload();
         }if(arrayP[3]==1 & arrayP[5]==1){
-            confirm('play again');
-            resetColor();
-            EndGame = true;
+            confirm(winner + ' wins.' + ' play again?');
+            location.reload();
         }
     }
     
     if(arrayP[6]==1){
         if(arrayP[7]==1 & arrayP[8]==1){
-            confirm('play again');
-            resetColor();
-            EndGame = true;
+            confirm(winner + ' wins.' + ' play again?');
+            location.reload();
         }
-    }
-}
-
-function resetColor() {
-    playedXY = [0,0,0,0,0,0,0,0,0]
-    play = false;
-    y = null;
-    playedX = [0,0,0,0,0,0,0,0,0]
-    playedY = [0,0,0,0,0,0,0,0,0]
-    EndGame = false;
-    for(var i=0;i<9;i++){
-        displayX[i] = 0; 
-        displayY[i] = 0; 
-        document.getElementById(i).style.background = 'aqua';
     }
 }
 

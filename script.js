ec5026785c83b9b271cf9321612b75d0817e3498
Soufiane Elharem <44909504+Selharem@users.noreply.github.com>
2021-02-05 22:32:05 +0100

    Update script.js
    prevent changing color when clicking on a button two times

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-var Play = [0,0,0,0,0,0,0,0,0]
+var playedXY = [0,0,0,0,0,0,0,0,0]
 var play = false;
 var y;
 var playedX = [0,0,0,0,0,0,0,0,0]
 var playedY = [0,0,0,0,0,0,0,0,0]
 var EndGame = false;
 
-//document.getElementById('table').style.backgroundColor  = 'red';
-//document.getElementById('table').style.color  = 'red';
 document.getElementsByTagName('table').style.color = 'red';
 
 
@@ -25,39 +23,57 @@ function choose(x) {
 console.log(playedX);
 
 function displayX() {
-    document.getElementById(y).style.background = 'white';
-    play = !(play);
-    playedX[y]=1;
+    if(playedY[y]==0){
+        document.getElementById(y).style.background = 'white';
+        play = !(play);
+        playedX[y]=1;
+        playedXY[y]=1;
+    }
 }
 
 function displayY() {
-    document.getElementById(y).style.background = 'black';
-    play = !(play);
-    playedY[y]=1;
+    if(playedX[y]==0){
+        document.getElementById(y).style.background = 'black';
+        play = !(play);
+        playedY[y]=1;
+        playedXY[y]=1;
+    }
 }
 
 function verify(arrayP) {
     if(arrayP[0]==1){
         if(arrayP[1]==1 & arrayP[2]==1){
             confirm('play again');
-            document.getElementById(0).style.background = 'aqua';
+            //location.reload();
+            //document.getElementById('table').style.background = 'aqua';
             resetColor();
             var EndGame = true;
         }if(arrayP[3]==1 & arrayP[6]==1){
             confirm('play again');
-            document.getElementById('table').style.background = 'aqua';
+            //location.reload();
+            //document.getElementById('table').style.background = 'aqua';
+            resetColor();
             var EndGame = true;
         }if(arrayP[4]==1 & arrayP[8]==1){
             confirm('play again');
-            document.getElementById('table').style.background = 'aqua';
+            resetColor();
+            //document.getElementById('table').style.background = 'aqua';
+            //location.reload();
             var EndGame = true;
         }
     }
 }
 
 function resetColor() {
+    playedXY = [0,0,0,0,0,0,0,0,0]
+    play = false;
+    y = null;
+    playedX = [0,0,0,0,0,0,0,0,0]
+    playedY = [0,0,0,0,0,0,0,0,0]
+    EndGame = false;
     for(var i=0;i<9;i++){
         displayX[i] = 0; 
+        displayY[i] = 0; 
         document.getElementById(i).style.background = 'aqua';
     }
 }
